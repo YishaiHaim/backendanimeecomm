c9d9680578c54d8781a2d8509fb9b96998164300
YishaiHaim <97115878+YishaiHaim@users.noreply.github.com>
2022-11-27 17:04:43 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -172,21 +172,22 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
+ 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'images')
+# ]
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-     
-]
-
-
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static/media'),
+)
+ 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+STATIC_ROOT = BASE_DIR / 'static'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
